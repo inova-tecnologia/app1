34252a3084d0e28a3adba7728d73c4b6154a66f9
Carlos <carloseduardoleal10@gmail.com>
2023-04-05 14:36:39 -0300

    atualização 2.0

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,30 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhraseModel } from '../shared/phrase.model';
 import { PhrasesMock } from './phrase-mock';
-@Component({ 
+@Component({    
   selector: 'panel-app',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent {
-    
+
   public phrases: PhraseModel[] =  PhrasesMock;
   public instruction: string = "Translate the phrase:";
-  response1: string = "";
+  public response: string = ""; 
+  public round: number = 0;
+  public roundPhrase: PhraseModel;
 
-  constructor() { console.log(this.phrases) }
+  constructor() { 
+      this.roundPhrase = this.phrases[this.round];
+  }
 
   ngOnInit() {
 
   }      
 
   public updateResponse(response: Event): void {
-      this.response1 = (<HTMLInputElement>response.target).value;
-      console.log((this.response1));
+      this.response = (<HTMLInputElement>response.target).value;
+      console.log((this.response));
   } 
 
   public verifyResponse(): void {
-      console.log('Verificar resposta: ', this.response1);
-  }    
 
+    if(this.roundPhrase.phraseUS == this.response){
+       alert('A tradução está correta!');
+    }else{
+       alert('A tradução está incorreta!');
+    }
+
+    this.round++;
+    this.roundPhrase = this.phrases[this.round];
+    console.log('Verificar resposta: ', this.roundPhrase);
+  }    
+  
 }                   
